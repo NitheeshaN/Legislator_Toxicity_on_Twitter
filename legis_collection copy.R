3cf4104b92142ff7b5891b303aed4a0d16ed7770
NitheeshaN <50181380+NitheeshaN@users.noreply.github.com>
2023-10-12 15:06:15 -0400

    Update legis_collection copy.R

--- legis_collection copy.R
+++ legis_collection copy.R
@@ -6,11 +6,11 @@ library(readr)
 
 con <- DBI::dbConnect(
   RPostgres::Postgres(),
-  dbname = "tweets",
-  host = "dapr.psu.edu",
-  port = 5432,
-  user = "api",
-  password = "d@pru53r"
+  dbname = "",
+  host = "",
+  port = ",
+  user = "",
+  password = ""
 )
 
 # find the tables in the database
@@ -19,7 +19,7 @@ tables <- dbListTables(con)
 dbListFields(con,tables[2])
 
 ## get the legislator tweets sample
-legis_twts <- dbGetQuery(con, "SELECT * FROM legislator_tweets WHERE created_date BETWEEN '2021-01-01' AND '2021-01-31'")
+legis_twts <- dbGetQuery(con, "SELECT * FROM "Data Table Name" WHERE created_date BETWEEN '2021-01-01' AND '2021-01-31'")
 
 
 #save
@@ -29,7 +29,7 @@ saveRDS(legis_twts, "legis_twts.rds")
 #start_tweets = "2021-01-01T00:00:00Z",
 #end_tweets = "2021-02-01T00:00:00Z"
 
-#query <- paste("SELECT * FROM legislator_tweets WHERE created_date BETWEEN '2021-01-01' AND '2022-12-31' AND text LIKE '%@AndresRomeroNM%'", handle_or, ";", sep  = "")
+#query <- paste("SELECT * FROM "Data Table Name" WHERE created_date BETWEEN '2021-01-01' AND '2022-12-31' AND text LIKE '%@AndresRomeroNM%'", handle_or, ";", sep  = "")
 
 #run 
 #twts_endo <- dbGetQuery(con,query)
